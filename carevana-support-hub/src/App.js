532770e5ff2929b8935404ceca7fd6ca8a4bb439
DishaMehra06 <2023nitsgr156@nitsri.ac.in>
2025-09-29 17:51:44 +0530

    Update App.js

--- carevana-support-hub/src/App.js
+++ carevana-support-hub/src/App.js
@@ -1,26 +1,43 @@
-import './App.css';
-import Banner from './components/banner/Banner';
-import Doctors from './components/doctors/Doctors';
-import Facilities from './components/facilities/Facilities';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Partners from './components/partners/Partners';
-import Services from './components/services/Services';
-import Testimonials from './components/testimonials/Testimonials';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Banner from "./components/banner/Banner";
+import Doctors from "./components/doctors/Doctors";
+import Facilities from "./components/facilities/Facilities";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Partners from "./components/partners/Partners";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import AzureScan from "./components/azure1/AzureScan"; // 👈 import your Azure Scan component
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Banner />
-      <Services />
-      <Doctors />
-      <Partners />
-      <Facilities />
-      <Testimonials />
-      <Footer />
-    </>
+      <Routes>
+        {/* Default Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <Services />
+              <Doctors />
+              <Partners />
+              <Facilities />
+              <Testimonials />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Azure Scan Page */}
+        <Route path="/azure-scan" element={<AzureScan />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
